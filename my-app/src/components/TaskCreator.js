3179302e740f0acf2295b9769da710d414003f81
Emily Yu <eyudeveloper@gmail.com>
2020-11-09 03:57:38 -0800

    binding for button

--- my-app/src/components/TaskCreator.js
+++ my-app/src/components/TaskCreator.js
@@ -3,14 +3,20 @@ import React from 'react';
 class TaskCreator extends React.Component {
     constructor(props) {
         super(props);
+        this.createTask= this.createTask.bind(this);
     }
+
+    createTask() {
+        console.log("asdf")
+    }
+
     render() {
         return (
           <div>
               <h2>new task</h2>
               <input type="text" id="fname" placeholder="task name"></input>
               <input type="text" id="fname" placeholder="time to last (in seconds)"></input>
-              <input type="button" value="done"></input>
+              <input type="button" value="done" onClick={this.createTask}></input>
           </div>
         )
       }
