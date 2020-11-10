7e1bb4507955569a830a49631e77158898681650
Emily Yu <eyudeveloper@gmail.com>
2020-11-10 03:57:57 -0800

    actual data for countdown stuff

--- my-app/src/components/TaskCreator.js
+++ my-app/src/components/TaskCreator.js
@@ -1,22 +1,51 @@
 import React from 'react';
 
+const DURATION_HEADER = "duration (in seconds)";
 class TaskCreator extends React.Component {
     constructor(props) {
         super(props);
         this.createTask= this.createTask.bind(this);
+        this.updateParams= this.updateParams.bind(this);
+        this.state = {
+            taskName: "",
+            timeRemaining: "",
+        }
     }
 
     createTask() {
         // access task from parent
-        this.props.addTask("asdf", "200")
+        if (this.state.taskName == "" && this.state.timeRemaining == "") {
+            console.log("error")
+        }
+        else {
+            this.props.addTask(this.state.taskName, this.state.timeRemaining)
+        }
+    }
+    updateParams(event) {
+        // this.setState = {
+        //     taskName: event.target.value,
+        //     timeRemaining
+        // }
+        if (event.target.placeholder == "title") {
+            this.setState({
+                taskName: event.target.value
+            })
+        }
+        else {
+            this.setState({
+                timeRemaining: event.target.value
+            })
+        }
+        console.log(event.target.placeholder)
+        console.log(JSON.stringify(this.state))
     }
 
     render() {
         return (
           <div>
               <h2>new task</h2>
-              <input type="text" id="fname" placeholder="task name"></input>
-              <input type="text" id="fname" placeholder="time to last (in seconds)"></input>
+              <input type="text" placeholder="title" onChange={this.updateParams}></input>
+              <input type="text" placeholder={DURATION_HEADER} onChange={this.updateParams}></input>
               <input type="button" value="done" onClick={this.createTask}></input>
           </div>
         )
