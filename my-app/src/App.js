e36b9b479feb725516b41d83d8398c595f6c4861
Emily Yu <eyudeveloper@gmail.com>
2020-11-10 03:57:54 -0800

    pass new tasks to tasklist

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,36 @@ import Task from './components/Task';
 import TaskList from './components/TaskList'
 import TaskCreator from './components/TaskCreator'
 import React from 'react';
+const Child = ({ number }) => <h1>{number}</h1>;
+const Child2 = ({ taskName, timeRemaining }) => <Task name={taskName} timeRemaining={timeRemaining}/>;
 
-function App() {
-  let tasks = [];
-  
-  const addTask = (taskName, timeRemaining) => {
+class App extends React.Component{    
+  constructor(props){
+       super(props);
+       this.state = { tasks: [], y: 0 };
+       this.addTask = this.addTask.bind(this)
+  }    
+
+  addTask = (taskName, timeRemaining) => {
     console.log(taskName, timeRemaining)
-    // todo: update tasklist component + datastructure
+    
+    // update tasklist datastructure
+    this.state.tasks.push({taskName, timeRemaining})
+    this.setState({tasks:this.state.tasks})
+    console.log(this.state.tasks)
+
+    // update tasklist item
   }
-  return (
-    <div className="App">
-        <TaskCreator addTask={addTask}/>
-        <TaskList/>
-    </div>
-  );
+
+  render() {
+    return (
+      <div className="App">
+          <TaskCreator addTask={this.addTask}/>
+          <TaskList tasks={this.state.tasks}/>
+          <Child number={2}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- my-app/src/components/TaskList.js
+++ my-app/src/components/TaskList.js
@@ -3,13 +3,25 @@ import Task from './Task';
 class TaskList extends React.Component {
     constructor(props) {
         super(props);
+        console.log("tasklist", this.state)
+        this.state = {
+            data: props.tasks
+        };
+    }
+    componentWillReceiveProps(props){
+        // Desired operations: ex setting state
+        console.log("updated")
+        console.log("Data: ", this.state.data)
     }
     render() {
         return (
           <div>
             <h2>tasklist</h2>
-            <Task name="asdf" timeRemaining="200"/>
-            <Task name="asdf2" timeRemaining="2000"/>
+            {
+                this.state.data.map(item => {
+                    return <Task name={item.taskName} timeRemaining={item.timeRemaining}/>
+                })
+            }
           </div>
         )
       }
